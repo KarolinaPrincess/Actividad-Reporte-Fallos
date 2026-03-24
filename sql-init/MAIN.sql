9e6906f9e9959f11cbd79716126823c24d961859
KarolinaPrincess <carlosvicente551@gmail.com>
2026-03-24 00:41:46 -0600

    feat: implementar script orquestador principal para despliegue secuencial

---
+++ sql-init/MAIN.sql
@@ -0,0 +1,10 @@
+-- ============================================================
+-- ORQUESTADOR DE INICIALIZACIÓN (Main)
+-- ============================================================
+
+\i V1_23032026_CREATE_TABLES.sql
+\i V1_23032026_Funtions_triggers.sql
+\i V1_23032026_ALTER_TABLES.sql
+\i V1_23032026_CREATE_INDEX.sql
+\i V1_24032026_VACUUM_ANALYZE.sql
+\i V1_24032026_COMMENT_ON.sql
